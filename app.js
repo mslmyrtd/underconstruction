7ff810e587e58d7e221e9f0d83ee70c9de7c855f
mslmyrtd <mslmyrtd@gmail.com>
2022-02-21 17:10:06 +0300

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var countDownDate= new Date("July 16, 2022 00:00:00").getTime()
+var countDownDate= new Date("Feb 28, 2022 00:00:00").getTime()
 var x =setInterval(function(){
     var now =new Date().getTime()
     var distance =countDownDate - now;
